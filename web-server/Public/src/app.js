6d1197dccf93edf34da7c300b52e69012edba281
https://github.com/Sadaf1024/nodejs.git <sibghat.baig@brainplow.com>
2020-01-08 19:42:18 +0500

    1/8 office

--- web-server/Public/src/app.js
+++ web-server/Public/src/app.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
+const utils = require('../../../Utils/app')
 
+// utils.geoCode()``
 const app = express()
 
 // console.log(path.join(__dirname,'../Public'))
 
 const publicDirectry = path.join(__dirname)
-const views = path.join(__dirname,'../views')
+const views = path.join(__dirname,'../templates/views')
+const partials = path.join(__dirname,'../templates/partials')
+
 console.log(views)
+
 app.set('view engine','hbs')
 app.set('views', views)
+hbs.registerPartials(partials)
 
 app.use(express.static(publicDirectry))
 
@@ -32,6 +38,19 @@ app.get('/about',(req, res) => {
         age: '27'
 
 })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+    res.send({
+        name:'Sadaf Ali'
+    })
+}else{
+    res.send({
+        Search: 'You Enter your search'
+    })
+}
+
 })
 
 // app.use()
